06a69e744d26408dfa4588847a0766b9f48bf0fd
johnny <johnnyliao06@gmail.com>
2024-10-19 14:43:09 -0700

    fixed errors and added shadcn + tailwindcss

--- frontend/src/components/pages/home/index.jsx
+++ frontend/src/components/pages/home/index.jsx
@@ -1,24 +1,16 @@
-import WalletSection from "./wallet-section";
 import styles from "./styles.module.css";
 import { useWallet } from "@/hooks/useWallet";
-import InfoSection from "./info-section";
-import WallSection from "./wall-section";
 
 export default function Home() {
-    const { walletConnectionStatus } = useWallet();
+  const { walletConnectionStatus } = useWallet();
 
-    return (
-        <div className={styles.home}>
-            <section className={styles.col1}>
-                <WalletSection />
-            </section>
+  return (
+    <div className={styles.home}>
+      <section className={styles.col1}>Wallet</section>
 
-            <section className={styles.col2}>
-                {walletConnectionStatus === "connected"
-                    ? <WallSection />
-                    : <InfoSection />
-                }
-            </section>
-        </div>
-    )
+      <section className={styles.col2}>
+        {walletConnectionStatus === "connected" ? "Connected" : "Not Connected"}
+      </section>
+    </div>
+  );
 }
